372125f6b4c73999802f81b8f9832b1d407bf303
ercnaydin1 <ercnaydin1@gmail.com>
2020-08-09 23:50:11 +0200

    ikinci projemin ilk commiti

---
+++ src/git_project_ikinci/Deneme.java
@@ -0,0 +1,15 @@
+package git_project_ikinci;
+
+public class Deneme {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Ikinci git projem");
+		System.out.println("Ikinci satir kodum");
+		System.out.println("remote degisiklik");
+		System.out.println("Ercan");
+		System.out.println("cakisma qa");
+		
+	}
+
+}
